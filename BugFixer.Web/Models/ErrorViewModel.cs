ce157e1499974c52b176e28b657efa55cd34fe60
Saeed_Pasha <hajizadesaeed.78@gmail.com>
2023-10-25 06:40:16 +0330

    Initialize Project

---
+++ BugFixer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BugFixer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
