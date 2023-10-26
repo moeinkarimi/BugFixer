b8015e51e786160e1250c0a4b7f5b0ac7ebe16a8
Saeed_Pasha <hajizadesaeed.78@gmail.com>
2023-10-26 09:19:40 +0330

    user repository were created

---
+++ BugFixer.Data/Repository/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using BugFixer.Data.Context;
+using BugFixer.Domain.Interfaces;
+using BugFixer.Domain.Models.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugFixer.Data.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly BugFixerDBContext _ctx;
+        public UserRepository(BugFixerDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+        public async Task CreateAsync(User user)
+        {
+            await _ctx.AddAsync(user);
+        
+        }
+
+        public void Delete(User user)
+        {
+            _ctx.Remove(user);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _ctx.Users.ToListAsync();
+        }
+
+        public async Task<User> GetAsync(int userId)
+        {
+            return await _ctx.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task SaveChangeAsync()
+        {
+            await _ctx.SaveChangesAsync();
+        }
+
+        public void Update(User user)
+        {
+            _ctx.Update(user);
+        }
+    }
+}
---
+++ BugFixer.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using BugFixer.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugFixer.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task SaveChangeAsync();
+        #region Admin
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> GetAsync(int userId);
+        Task CreateAsync(User user);
+        void Update(User user);
+        void Delete(User user);
+
+        #endregion
+    }
+}
