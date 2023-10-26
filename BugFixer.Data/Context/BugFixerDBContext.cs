ecc1ee9560a63e0ea3238a1b53a4b75cd4035339
Saeed_Pasha <hajizadesaeed.78@gmail.com>
2023-10-26 08:51:29 +0330

    BugFixer Context were Created

---
+++ BugFixer.Data/Context/BugFixerDBContext.cs
@@ -0,0 +1,28 @@
+﻿using BugFixer.Domain.Models.Role;
+using BugFixer.Domain.Models.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugFixer.Data.Context
+{
+    public class BugFixerDBContext : DbContext
+    {
+        public BugFixerDBContext(DbContextOptions<BugFixerDBContext> options) : base(options)
+        {
+
+        }
+
+        #region User
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Persmissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+
+        #endregion
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
