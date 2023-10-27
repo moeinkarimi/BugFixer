b74b6ed147279ab7423d77cd05ab9460cab4a339
Alireza <alirezaghafari0101@gmail.com>
2023-10-27 14:41:59 +0330

    inject services and repositories

---
+++ BugFixer.IOC/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using BugFixer.Application.Services.Implementations;
+using BugFixer.Application.Services.Interfaces;
+using BugFixer.Data.Repository;
+using BugFixer.Domain.Interfaces;
+using EShop.Application.Convertors;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugFixer.IOC
+{
+    public class DependencyContainer
+    {
+        public static void UserServices(IServiceCollection service)
+        {
+            service.AddScoped<IUserRepository, UserRepository>();
+            service.AddScoped<IUserService, UserService>();
+
+            service.AddScoped<IAccountService, AccountService>();
+
+            service.AddScoped<IViewRenderService, RenderViewToString>();
+        }
+    }
+}
--- BugFixer.Web/Program.cs
+++ BugFixer.Web/Program.cs
@@ -1,4 +1,5 @@
 using BugFixer.Data.Context;
+using BugFixer.IOC;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BugFixerDBContext>(options =>
      options.UseSqlServer(builder.Configuration.GetConnectionString("BugFixerConnection"))
     );
+
+#region Injections
+DependencyContainer.UserServices(builder.Services);
+#endregion
 
 var app = builder.Build();
 
