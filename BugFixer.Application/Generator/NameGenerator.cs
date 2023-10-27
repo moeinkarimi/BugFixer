d916e7ff8a4a3a62ce71db247723cd915fdb16be
Alireza <alirezaghafari0101@gmail.com>
2023-10-27 14:41:26 +0330

    add utilities

---
+++ BugFixer.Application/Generator/NameGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Application.Generator
+{
+    public class NameGenerator
+    {
+
+        public static string GenerateUniqCode()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+        public static string GenerateUnipNDigitCode(int randomDigit)
+        {
+            // Number of digits for random number to generate
+
+            int _max = (int)Math.Pow(10, randomDigit);
+            Random _rdm = new Random();
+            int _out = _rdm.Next(0, _max);
+
+            while (randomDigit != _out.ToString().ToArray().Distinct().Count())
+            {
+                _out = _rdm.Next(0, _max);
+            }
+            return _out.ToString();
+        }
+    }
+}
+
---
+++ BugFixer.Application/Senders/SendEmail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Application.Senders
+{
+    public class SendEmail
+    {
+        public static void Send(string to, string subject, string body)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587);
+            mail.From = new MailAddress("hajizadesaeed.78@gmail.com", "یکتاکالا");
+            mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            //System.Net.Mail.Attachment attachment;
+            // attachment = new System.Net.Mail.Attachment("c:/textfile.txt");
+            // mail.Attachments.Add(attachment);
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("hajizadesaeed.78@gmail.com", "rhxkzovdxgnhsfyv");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+
+        }
+    }
+}
