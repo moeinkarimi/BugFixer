db40204b0f623389b4e6132bed769f2dacc571dd
Alireza <alirezaghafari0101@gmail.com>
2023-10-27 13:02:06 +0330

    add connection string and init db

--- BugFixer.Web/Program.cs
+++ BugFixer.Web/Program.cs
@@ -1,7 +1,14 @@
+using BugFixer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BugFixerDBContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("BugFixerConnection"))
+    );
 
 var app = builder.Build();
 
