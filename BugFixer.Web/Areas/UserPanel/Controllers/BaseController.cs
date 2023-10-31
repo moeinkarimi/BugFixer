915d396d16c2fefb65c1b16d691901f46619d094
Alireza <alirezaghafari0101@gmail.com>
2023-10-31 14:08:44 +0330

    add userpanel layout

---
+++ BugFixer.Web/Areas/UserPanel/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugFixer.Web.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class BaseController : Controller
+    {
+       
+    }
+}
